50ae0a149cf3fcbd3431c20246d9a137f5f0a5c1
mariavihhteixeira <maria.teixeira03@escola.pr.gov.br>
2026-06-18 09:38:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-const botoes=document.querySelectAll (".botao");
-
-for(let i=0; i<botoes.length; i++){
-botoes[i].onclick=function(){
-for (let j=0;j<botoes.length;j++){
-botoes[j].classList.remove("ativo");
-}
-botoes[i].classList.add("ativo");
-};
-
-
-
-}
 
 const botoes = document.querySelectorAll(".botoes");
 const textos = document.query.SelectorAll(".aba-conteudo");
